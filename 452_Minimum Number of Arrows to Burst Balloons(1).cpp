6965d2af5e54d47dfa5d2867ee6f72b10ea11cd5
Dewansh Khandelwal <dewansh3255@gmail.com>
2023-01-05 14:21:24 +0530

    second commit

--- 452_Minimum Number of Arrows to Burst Balloons(1).cpp
+++ 452_Minimum Number of Arrows to Burst Balloons(1).cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     static bool compare(vector<int> &a, vector<int> &b)
-    {
         return a[1]<b[1];
-    }
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end(), compare);
         int count = 1;
